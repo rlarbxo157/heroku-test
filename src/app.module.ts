549f5f2c5b66140de31758f86225b528b702e76d
gtkim <gtkim@clamos.io>
2022-10-17 10:01:20 +0900

    db setting change

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,15 @@ import { BoardService } from './board/board.service';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type:'postgres',
-    url: process.env.DATABASE_URL || 'postgres://uzvklfwfmfjdee:1999096aa6e324144f0a8eb0c5ce2e3cbf7d1d6a6236d3a2700c755a4e0a6d24@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d5nnq0f4tc2eqk',
+    // url: process.env.DATABASE_URL || 'postgres://uzvklfwfmfjdee:1999096aa6e324144f0a8eb0c5ce2e3cbf7d1d6a6236d3a2700c755a4e0a6d24@ec2-44-210-228-110.compute-1.amazonaws.com:5432/d5nnq0f4tc2eqk',
+    ...(process.env.DATABASE_URL ? {url: process.env.DATABASE_URL} : {
+        host:'ec2-44-210-228-110.compute-1.amazonaws.com',
+        port:5432,
+        username:'uzvklfwfmfjdee',
+        password:'1999096aa6e324144f0a8eb0c5ce2e3cbf7d1d6a6236d3a2700c755a4e0a6d24',
+        database:'d5nnq0f4tc2eqk'
+    }),
+
     extra: {
       ssl: {
         rejectUnauthorized :  false
