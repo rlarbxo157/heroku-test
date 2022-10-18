98b060e08e407afa619b36760c0ba2af77c21d88
gtkim <gtkim@clamos.io>
2022-10-18 09:24:45 +0900

    댓글 controller swagger operation 적용

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,18 +1,25 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import {ApiTags, ApiOperation, ApiCreatedResponse, ApiBody} from '@nestjs/swagger';
 
 @Controller('comment')
+@ApiTags('댓글 control Api')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post()
+  @ApiBody({type:CreateCommentDto})
+
+  @ApiOperation({summary:'댓글 생성Api.',description:'댓글 생성'})  //summary 는 보이는곳
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentService.create(createCommentDto);
   }
 
   @Get()
+  @ApiOperation({summary:'댓글 조회',description:'댓글 조회'})  //summary 는 보이는곳
   findAll() {
     return this.commentService.findAll();
   }
